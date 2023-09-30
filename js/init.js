2d4535128901e6fbe6b4898083bf825e46b5e5ac
diegoalgibar <diegoalgibar123@gmail.com>
2023-09-30 15:31:13 -0300

    Modo oscuro terminado

--- js/init.js
+++ js/init.js
@@ -7,6 +7,7 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 const logOutBtn=document.getElementById("logOut");
+const alternarTemaBTN=document.getElementById("alternarTema");
 let user=""
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
@@ -55,6 +56,26 @@ function checkLog() {
 }
 document.addEventListener("DOMContentLoaded", function() {
 
+  const listas=document.getElementsByClassName("list-group");
+  if(!window.location.href.includes("index")){
+  if (JSON.parse(localStorage.getItem("modo"))) {
+    document.body.classList.toggle("modo-noche");
+    alternarTemaBTN.classList.toggle("btn-light")
+    listas[0].classList.toggle("list-group-dark");
+  }
+    alternarTemaBTN.addEventListener("click",()=>{
+      if (JSON.parse(localStorage.getItem("modo"))) {
+        localStorage.setItem("modo",false);
+        alternarTemaBTN.classList.toggle("btn-light")
+      } else {
+        localStorage.setItem("modo",true);
+        alternarTemaBTN.classList.toggle("btn-light")
+      }      
+      listas[0].classList.toggle("list-group-dark");
+      document.body.classList.toggle("modo-noche");
+    })
+  }
+    
 checkLog();
 //Funcionalidad del botón de cerrar sesion, limpia el usuario de sessionStorage y localStorage, a continuación lo redirigimos a la página de login
 logOutBtn.addEventListener("click",()=>{
