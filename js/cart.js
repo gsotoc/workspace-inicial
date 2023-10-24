f530f90c5158b675956a5b139427491bab0337a1
gsotoc <germansoto7878@gmail.com>
2023-10-24 16:52:31 -0300

    costos de envio
    faltan ajustar algunas cosas pero quedó funcional

--- js/cart.js
+++ js/cart.js
@@ -1,5 +1,4 @@
-// hago el fetch a la api para obtener los productos del carrito del usuario 25801 - pauta 1
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded",() => {
     let htmlcontentToAppend = `<div  class="list-group-item list-group">     
     <h2 class"center"> Mi carrito de compras </h2>    
     
@@ -14,24 +13,46 @@ document.addEventListener("DOMContentLoaded", async () => {
     </tr>
     </thead>
     <tbody>`
-    const user = sessionStorage.getItem("user") || localStorage.getItem('user');
+    user = sessionStorage.getItem("user") || localStorage.getItem('user');
     let carritoLocalStorage = JSON.parse(localStorage.getItem('carritos'));
     let carritoUsuario = carritoLocalStorage[user]
+    let subtotal = 0;
+    let costoEnvio = 0;
+    let envio = 0.15;
+
     for (const producto in carritoUsuario) {
         const prod = carritoUsuario[producto];
-        let subtotal = prod.count * prod.unitCost;
+        let prodSubtotal = prod.count * prod.unitCost;
         htmlcontentToAppend += `<tr id="${producto}">
                                 <td><img src="${prod.image}" width="100"></td>
                                 <td>${prod.name}</td>
                                 <td><input type="number" value="${prod.count}" min="1"></input></td>
                                 <td>${prod.currency} ${prod.unitCost} </td>
-                                <td>${prod.currency} <span>${subtotal}</span></td>
+                                <td>${prod.currency} <span>${prodSubtotal}</span></td>
                                 </tr>`;
+    
+        //Creo el contenido a ser ingresado en el total
+        subtotal += prodSubtotal;
+        costoEnvio = subtotal*envio;
+        let total = costoEnvio+subtotal;
+        htmlTotalContentToAppend = `<h4 class="mt-3">Total</h4>
+                                    <div class="row border d-flex">
+                                        <h5 class="d-inline-flex pt-1">Subtotal</h5>
+                                        <span class="text-end"">USD ${subtotal}</span>
+                                    </div>
+                                    <div class="row border" id="costoEnvio">
+                                        <h5 class="d-inline-flex pt-1">Costo de envío</h5>
+                                        <span class="text-end">USD ${costoEnvio}</span>
+                                    </div>
+                                    <div class="row border">
+                                        <h5 class="d-inline-flex pt-1">Total</h5>
+                                        <span class="text-end">USD ${total}</span>
+                                    </div>`;
+        document.getElementById("total").innerHTML = htmlTotalContentToAppend   
     }
-    htmlcontentToAppend += `</tbody>
-</table>
-</div>`
-    document.getElementById('shoppingCart').innerHTML = htmlcontentToAppend
+    document.getElementById('shoppingCart').innerHTML = htmlcontentToAppend;
+
+
     for (const producto in carritoUsuario) {
         const prod = carritoUsuario[producto];
         let prodHTML=document.getElementById(producto)
@@ -41,6 +62,48 @@ document.addEventListener("DOMContentLoaded", async () => {
             carritoUsuario[producto].count=parseInt(inputProd.value);
             carritoLocalStorage[user]=carritoUsuario;
             localStorage.setItem('carritos',JSON.stringify(carritoLocalStorage));
-        })
-    }
-});
+
+            // Funcionalidad del total
+            let carritoTotalLocalStorage = JSON.parse(localStorage.getItem('carritos'));
+            let carritoTotal = carritoTotalLocalStorage[user];
+            subtotalProducto=0;
+            for (const producto in carritoTotal){
+                const totalProd = carritoTotal[producto];
+                subtotalProducto += totalProd.unitCost*totalProd.count;
+                costoEnvio = Math.round(subtotalProducto*envio);
+                total = subtotalProducto + costoEnvio;
+            }
+            totalContentToAppend = `<h4 class="mt-3">Total</h4>
+                                    <div class="row border d-flex">
+                                        <h5 class="d-inline-flex pt-1">Subtotal</h5>
+                                        <span class="text-end">${subtotalProducto}</span>
+                                    </div>
+                                    <div id="costoEnvio" class="row border">
+                                        <h5 class="d-inline-flex pt-1">Costo de envío</h5>
+                                        <span class="text-end">${costoEnvio}</span>
+                                    </div>
+                                    <div class="row border">
+                                        <h5 class="d-inline-flex pt-1">Total</h5>
+                                        <span class="text-end">${total}</span>
+                                    </div>`;
+            document.getElementById("total").innerHTML = totalContentToAppend;
+
+            let datosCarrito = {
+                subtotal: `${subtotalProducto}`,
+                costoEnvio: `${costoEnvio}`,
+                total: `${total}`
+            }
+            localStorage.setItem('datosCarrito', JSON.stringify(datosCarrito));
+        })  
+        
+        let shippingCart = JSON.parse(localStorage.getItem('datosCarrito'));
+        console.log(shippingCart)
+        let botonesRadio = document.querySelectorAll('[name="flexRadioDefault"]');
+        botonesRadio.forEach(boton => boton.addEventListener("click",(e)=>{
+        document.getElementById("costoEnvio").innerHTML = "";
+        envio = e.target.value;
+        shippingCost = Math.round(shippingCart.subtotal*envio);
+        document.getElementById("costoEnvio").innerHTML = `<h5 class="d-inline-flex pt-1">Costo de envío</h5>
+                                                            <span class="class="text-end"">USD ${shippingCost}</span>`;        
+    }));
+}});
