6d09a69d518ee160c3c0326084c1db768f94a291
diegoalgibar <diegoalgibar123@gmail.com>
2023-10-26 18:53:17 -0300

    eliminar productos

--- js/cart.js
+++ js/cart.js
@@ -30,7 +30,7 @@ document.addEventListener("DOMContentLoaded",() => {
                                 <td><input type="number" value="${prod.count}" min="1"></input></td>
                                 <td>${prod.currency} ${prod.unitCost} </td>
                                 <td>${prod.currency} <span>${prodSubtotal}</span></td>
-                                <td><button type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                                <td><button onclick="eliminarProducto(${producto})" type="button" class="btn btn-danger"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
                                 <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
                                 <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
                               </svg></button></td>
@@ -111,4 +111,13 @@ document.addEventListener("DOMContentLoaded",() => {
         document.getElementById("costoEnvio").innerHTML = `<h5 class="d-inline-flex pt-1">Costo de envío</h5>
                                                             <span class="text-end">USD ${shippingCost}</span>`;        
     }));
+
 }});
+function eliminarProducto(idproducto) {
+    let carritoLocalStorage = JSON.parse(localStorage.getItem('carritos'));
+    let carritoUsuario = carritoLocalStorage[user]
+    delete carritoUsuario[idproducto];
+    carritoLocalStorage[user]=carritoUsuario
+    localStorage.setItem('carritos',JSON.stringify(carritoLocalStorage))
+    location.reload();
+}
