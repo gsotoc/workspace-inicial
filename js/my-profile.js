391eef1328e9f6358fcaeee4d1ca0fd42539237f
diegoalgibar <diegoalgibar123@gmail.com>
2023-11-09 19:25:49 -0300

    desafiate

--- js/my-profile.js
+++ js/my-profile.js
@@ -4,6 +4,8 @@ const apellidoTxt=document.getElementById("apellido");
 const apellido2Txt=document.getElementById("segundo_apellido");
 const emailTxt=document.getElementById("emailUser");
 const telefonoTxt=document.getElementById("telefono");
+const imgFile=document.getElementById("profileImg");
+const imagenMostrada=document.getElementById("imagenMostrada");
 (function () {
     'use strict'
     var forms = document.querySelectorAll('.needs-validation')
@@ -38,6 +40,10 @@ function cargarDatosUsuarioEnForm() {
         apellidoTxt.value=objUsuario[user].apellido
         apellido2Txt.value=objUsuario[user].apellido2
         telefonoTxt.value=objUsuario[user].telefono
+        if (objUsuario[user].imagen!="") {
+          imagenMostrada.src=objUsuario[user].imagen
+          imagenMostrada.style.display="block";
+        }
     }
 }
 function guardarDatosFormulario(){
@@ -49,6 +55,7 @@ function guardarDatosFormulario(){
         apellido:apellidoTxt.value,
         apellido2:apellido2Txt.value,
         telefono:telefonoTxt.value,
+        imagen:imagenMostrada.src
     }
     if (emailTxt.value!=user) {
         objUsuario[emailTxt.value]=objUsuario[user]
@@ -56,4 +63,37 @@ function guardarDatosFormulario(){
     }
     localStorage.setItem("user",emailTxt.value)
     localStorage.setItem("userInfo",JSON.stringify(objUsuario))
+}
+
+function convertirIMGB64() {
+  var input = document.getElementById('profileImg');
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+          var base64Image = e.target.result;
+          var image = new Image();
+
+          image.onload = function() {
+              // Crear un canvas y reescalar la imagen a 100x100 píxeles
+              var canvas = document.createElement('canvas');
+              canvas.width = 100;
+              canvas.height = 100;
+              var ctx = canvas.getContext('2d');
+              ctx.drawImage(image, 0, 0, 100, 100);
+
+              // Convertir el canvas a una cadena Base64
+              var resizedBase64Image = canvas.toDataURL('image/jpeg');
+              // Mostrar la imagen reescalada en la página
+              imagenMostrada.src = resizedBase64Image;
+              imagenMostrada.style.display="block"
+              
+          };
+
+          image.src = base64Image;
+          
+      };
+
+      reader.readAsDataURL(input.files[0]);
+  }
 }
