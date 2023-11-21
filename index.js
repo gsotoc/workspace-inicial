4b35beb5a9d9cf56529150b87eaef3636a4b385d
gsotoc <germansoto7878@gmail.com>
2023-11-21 19:10:44 -0300

    cambio en la ruta

--- index.js
+++ index.js
@@ -1,36 +1,40 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const fs = require('fs');
+
 const port = 3000;
 
 const dataFolderPath = path.join(__dirname);
 
 //Ruta para obtener los archivos JSON
-app.get('/cats',(req, res)=>{
-    const catsID = cat;
+app.get('/cats/:id',(req, res)=>{
+    const catsID = req.params.id;
     const filePath = path.join(dataFolderPath, 'cats', `${catsID}.json`);
-    res.json.sendFile(filePath);
+    res.sendFile(filePath);
 });
 
-app.get('/cart',(req, res)=>{
-    const buyId = buy;
+//Funciona
+app.get('/cart/:id',(req, res)=>{
+    const buyId = req.params.id;
     const filePath = path.join(dataFolderPath, 'cart', `${buyId}.json`);
     res.sendFile(filePath);
 });
 
+//Funciona
 app.get('/cats_products/:id',(req, res)=>{
     const catsproductsId = req.params.id;
     const filePath = path.join(dataFolderPath, 'cats_products', `${catsproductsId}.json`);
     res.sendFile(filePath);
 });
 
+//Funciona
 app.get('/products/:id',(req, res)=>{
     const productsId = req.params.id;
     const filePath = path.join(dataFolderPath, 'products', `${productsId}.json`);
     res.sendFile(filePath);
 });
 
+//Funciona
 app.get('/products_comments/:id',(req, res)=>{
     const product_commentsId = req.params.id;
     const filePath = path.join(dataFolderPath, 'products_comments', `${product_commentsId}.json`);
@@ -39,12 +43,12 @@ app.get('/products_comments/:id',(req, res)=>{
 
 
 app.get('/sell/:id',(req, res)=>{
-    const sellId = publish;
+    const sellId = req.params.id;
     const filePath = path.join(dataFolderPath, 'sell', `${sellId}.json`);
     res.sendFile(filePath);
 });
 
-
+//Funciona
 app.get('/user_cart/:id',(req, res)=>{
     const userId = req.params.id;
     const filePath = path.join(dataFolderPath, 'user_cart', `${userId}.json`);
