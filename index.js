b8fb0637d41f79e676e3431550796f66f6b3e3c3
gsotoc <germansoto7878@gmail.com>
2023-11-23 14:31:48 -0300

    pauta 1
    Completa la pauta 1, quedó funcional. Links y rutas actualizadas

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const port = 3000;
 
 const dataFolderPath = path.join(__dirname);
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+   });
 
 //Ruta para obtener los archivos JSON
 app.get('/cats/:id',(req, res)=>{
