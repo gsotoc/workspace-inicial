94be3f0619ca13bd2a3b19ffc00939722a2f05af
gsotoc <germansoto7878@gmail.com>
2023-11-26 01:04:59 -0300

    falta el cart

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const path = require('path');
 const jwt = require('jsonwebtoken');
+const mariadb=require('mariadb');
+const SECRET_KEY="CLAVE ULTRA SECRETA"
+
+
+const pool = mariadb.createPool({
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "eMercado",
+    connectionLimit: 5,
+  });
 
 
 const app = express();
 const port = 3000;
 const keys = require('./settings/keys');
+const { access } = require('fs');
 
 
 const dataFolderPath = path.join(__dirname);
@@ -28,21 +40,21 @@ app.get('/cats/:id',(req, res)=>{
     res.sendFile(filePath);
 });
 
-//Funciona
+
 app.get('/cart/:id',(req, res)=>{
     const buyId = req.params.id;
     const filePath = path.join(dataFolderPath, 'cart', `${buyId}.json`);
     res.sendFile(filePath);
 });
 
-//Funciona
+
 app.get('/cats_products/:id',(req, res)=>{
     const catsproductsId = req.params.id;
     const filePath = path.join(dataFolderPath, 'cats_products', `${catsproductsId}.json`);
     res.sendFile(filePath);
 });
 
-//Funciona
+
 app.get('/products/:id',(req, res)=>{
     const productsId = req.params.id;
     const filePath = path.join(dataFolderPath, 'products', `${productsId}.json`);
@@ -63,37 +75,96 @@ app.get('/sell/:id',(req, res)=>{
     res.sendFile(filePath);
 });
 
-//Funciona
+
+
 app.get('/user_cart/:id',(req, res)=>{
     const userId = req.params.id;
     const filePath = path.join(dataFolderPath, 'user_cart', `${userId}.json`);
     res.sendFile(filePath);
 });
 
+
 app.listen(port, ()=>{
     console.log(`Servidor corriendo en http://localhost:${port}`)
 });
 
 
-//Funcionalidad login
-app.post('/login', (req, res)=>{
+
+app.get("/login",async(req,res)=>{
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const rows = await conn.query(
+        "SELECT email,password, token FROM users"
+        );
     
-    if(req.body.usuario == "usuario" && req.body.contra == "contra"){
-        const payload = {
-            check: true
-        };
-        const token = jwt.sign(payload, app.get('key'), 
-        {
-            expiresIn: '1m'
-        });
-        res.json({
-            status: 'ok',
-            message: "Todo OK",
-            token: token
-        }); 
-    } else {
-        res.json({
-            message: "Error"
-        })
+        res.json(rows)
+        return 0
+    } catch (error) {
+        res.status(500).send({message:"Se rompio el servidor"})
+    } finally {
+        if (conn) conn.release(); //release to pool
+    }
+    return false;
+})
+
+app.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const connection = await pool.getConnection();
+
+        const result = await connection.query("SELECT * FROM users WHERE email = ? AND password = ?", [email, password]);
+
+        connection.release();
+
+        if (result.length > 0) {
+            const user = result[0];
+
+            res.status(200).json({ message: 'Inicio de sesión exitoso', token: user.token });
+        } else {
+            res.status(401).json({ message: 'Credenciales incorrectas' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en el servidor' });
     }
 });
+
+
+app.post('/registro', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const connection = await pool.getConnection();
+
+        const token = jwt.sign({ email }, SECRET_KEY, { expiresIn: '1h' });
+
+        await connection.query("INSERT INTO users (email, password, token) VALUES (?, ?, ?)", [email, password, token]);
+        connection.release();
+
+        res.status(200).json({ message: 'Usuario registrado con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Error en el servidor' });
+    }
+});
+
+const authorizeMiddleware = (req, res, next) => {
+
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Acceso no autorizado. Token no proporcionado.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, keys);
+        req.user = decoded;
+        alert('todo piola fiera');
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Acceso no autorizado. Token no válido.' });
+    }
+};
+
+app.use('/user_cart/:id', authorizeMiddleware);
